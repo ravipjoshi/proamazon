9b1a5e648b2b8fecf84eb11543e0a2f6af5687a8
Ravi Joshi <ravipiyushjoshi@gmail.com>
2019-08-08 17:01:23 -0400

    updated file with closing statement of connections

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+/*
+
+### Challenge #1: Customer View (Minimum Requirement)
+
+1. Create a MySQL Database called `bamazon`.
+
+2. Then create a Table inside of that database called `products`.
+
+3. The products table should have each of the following columns:
+
+   * item_id (unique id for each product)
+
+   * product_name (Name of product)
+
+   * department_name
+
+   * price (cost to customer)
+
+   * stock_quantity (how much of the product is available in stores)
+
+4. Populate this database with around 10 different products. (i.e. Insert "mock" data rows into this database and table). `done`
+
+5. Then create a Node application called `bamazonCustomer.js`. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+
+6. The app should then prompt users with two messages.
+
+   * The first should ask them the ID of the product they would like to buy.
+   * The second message should ask how many units of the product they would like to buy.
+
+7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+
+   * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
+
+8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
+   * This means updating the SQL database to reflect the remaining quantity.
+   * Once the update goes through, show the customer the total cost of their purchase.
+
+
+*/
+
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var p_id = 0;
@@ -22,19 +62,24 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    displayProducts();
+    welcomeScreen();
 
   });
  
+ function welcomeScreen(){
+     console.log("============================ Welcome to Proamazon Store ================================\n");
+     
+    displayProducts();
+ } 
 
 function displayProducts(){
     connection.query("SELECT * FROM products", function(err, res) {
         if(err) throw err;
 
-        console.log(`||===== ID =====||===== Product Name =====||===== Product Description =====||===== Department Name =====||===== Price =====      `);
+        console.log(`\n||===== ID =====||===== Product Name =====||===== Product Description =====||===== Department Name =====||===== Price =====      `);
        for(var i=0;i<res.length;i++)
        {
-        console.log(`||     ${res[i].item_id}     || ${res[i].product_name} || ${res[i].product_description} ||    ${res[i].department_name}     || ${res[i].price} `);
+        console.log(`\n||     ${res[i].item_id}     || ${res[i].product_name} || ${res[i].product_description} ||    ${res[i].department_name}     || ${res[i].price} `);
        }  
     });
 
@@ -130,13 +175,16 @@ function productAvailability(id,qty)
             ]
      })
      .then(function(choice){
-        switch (choice.) {
+        switch (choice.action) {
             case "Checkout - Print Invoice":
               displayOrder(id,qty,total);
+              connection.end();
               break;
       
             case "Cancel Order":
-              cancelOrder();
+              
+                cancelOrder();
+                connection.end();
               break;
             
             case "Exit":
